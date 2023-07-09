012fae3dd44ab9939bed64294a83fc292ed52039
Dmitry Lebedko <lebedko.dmitrii@gmail.com>
2023-07-09 11:05:39 +0200

    Comments + code refinements

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,29 +3,29 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    var rightPosition = 0
 
-    var guessLeft = ""
-    var secretLeft = ""
+    // count correct positions
+    val rightPosition = guess.zip(secret).count { (guessLetter, secretLetter) -> guessLetter == secretLetter }
 
-    guess.zip(secret)
-        .forEach { (guessLetter, secretLetter) ->
-            if (guessLetter == secretLetter) {
-                rightPosition++
-            } else {
-                guessLeft += guessLetter
-                secretLeft += secretLetter
-            }
-        }
+    // collect items, which are in wrong positions or guessed incorrectly
+    val (guessLeft, secretLeft) = guess.zip(secret)
+        .filter { (guessLetter, secretLetter) -> guessLetter != secretLetter }
+        .fold("" to "") { (guessLeft, secretLeft), (guessLetter, secretLetter)
+            -> (guessLeft + guessLetter) to (secretLetter + secretLeft)}
 
+    // count items left in the secret
     val secretLetterMap = secretLeft.groupingBy { it }.eachCount()
 
+    // count items left in the guess
     val guessLetterMap = guessLeft.groupingBy { it }.eachCount()
 
+    // count wrong positions
     val wrongPosition = guessLetterMap
-        .map { (key, value) ->  secretLetterMap
-            .getOrDefault(key, 0)
-            .coerceAtMost(value)}
+        .map { (guessLetter, guessLetterCount) ->  secretLetterMap
+            .getOrDefault(guessLetter, 0)
+            // take amount of items from the secret which positions were not guessed
+            // (limited by amount of items guessed successfully)
+            .coerceAtMost(guessLetterCount)}
         .fold(0, Integer::sum)
     return Evaluation(rightPosition, wrongPosition)
 }
